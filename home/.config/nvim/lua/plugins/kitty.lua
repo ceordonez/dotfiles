dc855dab86d478a1cb7249cdb68d210ded27288e
César Ordóñez <ceordonezv@gmail.com>
2025-11-10 11:13:00 +0100

    fixing keymapping for kitty and tmux movements

--- home/.config/nvim/lua/plugins/kitty.lua
+++ home/.config/nvim/lua/plugins/kitty.lua
@@ -1,4 +1,7 @@
 return {
 	"knubie/vim-kitty-navigator",
 	build = "cp ./*.py ~/.config/kitty/",
+	cond = function()
+		return vim.env.TMUX == nil
+	end,
 }
--- home/.config/nvim/lua/plugins/vim-tmux.lua
+++ home/.config/nvim/lua/plugins/vim-tmux.lua
@@ -1,10 +1,22 @@
 return {
 	"christoomey/vim-tmux-navigator",
-    -- vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
-    -- vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
-    -- vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
-    -- vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
-    keys = {
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+	event = VeryLazy,
+	cond = function()
+		return vim.env.TMUX ~= nil
+	end,
+	-- vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>'),
+	-- vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>'),
+	-- vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>'),
+	-- vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>'),
+	keys = {
 		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", "n", { silent = true } },
 		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", "n", { silent = true } },
 		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", "n", { silent = true } },
